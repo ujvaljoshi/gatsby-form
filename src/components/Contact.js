d83c29bb57087d98114733f6fcaa64fe0fbdf387
Ujval Joshi <contact@ujvaljoshi.com>
2018-10-23 08:58:10 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Contact = props => (
+  <section id="contact">
+    <div className="inner">
+      <section>
+        <form
+          action="/success"
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="bot-field" />
+          <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="6" />
+          </div>
+          <ul className="actions">
+            <li>
+              <input type="submit" value="Send Message" className="special" />
+            </li>
+            <li>
+              <input type="reset" value="Clear" />
+            </li>
+          </ul>
+        </form>
+      </section>
+    </div>
+  </section>
+)
+
+export default Contact
